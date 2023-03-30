006398c1adba3cf3e31cd8b9f35eeff7fc9bdb3f
Nadim <samnad14@gmail.com>
2023-03-30 16:04:04 -0400

    current working copy of spark script which uses json objects sent from data-source (new-current)

---
+++ streaming/app/spark_app1.py
@@ -0,0 +1,72 @@
+"""
+    This Spark app connects to the data source script running in another Docker container on port 9999, which feeds the
+    Spark app with a stream of data from the get requests made to the GitHub Search API.
+    The application receives the data stream, divides it into batches at an interval of 60 seconds (batch duration = 60 seconds),
+    and performs:
+        1. Compute the total number of collected repos since the start of the streaming application for each of the 3 programming languages (Pyhton, Java, C). Each repo should only be counted once.
+        2. Compute the number of collected repos with changes pushed during the last 60 seconds for all the repositories. Each repo should only be counted once.
+        3. compute the average number of stars of all the collected repos since the start of the streaming application for each of the 3 programming languages.
+        4. Find the top 10 most frequent words in the description of all the collected repos since the start of the streaming application for each of the 3 programming languages.
+        5. Print the analysis results of each batch to the console.
+"""
+import datetime
+import sys
+import requests
+import json
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import Row, SparkSession
+
+
+def aggregate_count(new_values, total_sum):
+    return sum(new_values) + (total_sum or 0)
+
+
+def get_sql_context_instance(spark_context):
+    if ('sqlContextSingletonInstance' not in globals()):
+        globals()['sqlContextSingletonInstance'] = SparkSession(spark_context)
+    return globals()['sqlContextSingletonInstance']
+
+
+def send_df_to_dashboard(df):
+    url = 'http://webapp:5000/updateData'
+    data = df.toPandas().to_dict('list')
+    requests.post(url, json=data)
+
+
+def process_rdd(time, rdd):
+    print("---------------- aggregate count of repos collected since start at time: %s ----------------" % str(time))
+    try:
+        sql_context = get_sql_context_instance(rdd.context)
+        row_rdd = rdd.map(lambda repo: Row(lang=repo[0], count=repo[1]))
+        results_df = sql_context.createDataFrame(row_rdd)
+        results_df.createOrReplaceTempView("repos")
+        results_df.show()
+        # new_results_df = sql_context.sql("select lang, count from repos")
+        # new_results_df.show()
+    except ValueError:
+        print("Waiting for data...")
+    except:
+        e = sys.exc_info()[0]
+
+
+if __name__ == '__main__':
+    DATA_SOURCE_IP = 'data-source'
+    DATA_SOURCE_PORT = 9999
+    sc = SparkContext(appName='GitHubSearch')
+    sc.setLogLevel('ERROR')
+    ssc = StreamingContext(sc, 60)
+    ssc.checkpoint('checkpoint_GitHubSearch')
+    data = ssc.socketTextStream(DATA_SOURCE_IP, DATA_SOURCE_PORT)
+    repos = data.map(lambda repo: json.loads(repo))
+    counts = repos.map(
+        lambda repo: (
+            repo['language'], 1
+        )
+    ).reduceByKey(lambda x, y: x + y).updateStateByKey(aggregate_count)
+    recent = repos.map(lambda repo: ("old" if (datetime.datetime.utcnow() - datetime.datetime.strptime(
+        repo['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")).total_seconds() > 60 else "recent", 1)).reduceByKey(lambda a, b: a+b)
+    counts.foreachRDD(process_rdd)
+    recent.pprint()
+    ssc.start()
+    ssc.awaitTermination()
